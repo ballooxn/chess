b22e6aa5e4fb8894cd9708be45e84ab921cb20c0
Lincoln Digby <lincolndigby0@gmail.com>
2024-11-19 18:34:29 -0600

    Make piece_moves

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,9 @@
+require_relative "display"
+require_relative "pieces/piece"
+require_relative "pieces/piece_moves"
+
 class Game
+  include Display
   def initialize(board = Array.new(8) { Array.new(8, "_") })
     @board = board
   end
