0363c1f061077e5aedd17b283238db52e70c127d
Lincoln Digby <lincolndigby0@gmail.com>
2024-11-25 16:18:02 -0600

    Add possible_move and return statement for player_input

--- lib/game.rb
+++ lib/game.rb
@@ -5,13 +5,19 @@ require_relative "player"
 
 class Game
   include Display
+  include PossibleMoves
 
   LETTER_TO_NUMBER = %w[a b c d e f g h]
+  LETTER_TO_PIECE = { "p" => "pawn", "n" => "knight", "k" => "king", "b" => "bishop", "q" => "queen", "r" => "rook" }
+  PIECE_MOVES = { "pawn" => { "white" => WHITE_PAWN_MOVES, "black" => BLACK_PAWN_MOVES }, "knight" => KNIGHT_MOVES, "bishop" => BISHOP_MOVES,
+                  "king" => KING_MOVES, "queen" => QUEEN_MOVES, "rook" => ROOK_MOVES }
 
   def initialize(board = Array.new(8) { Array.new(8, "_") })
     @board = board
     @player1 = Player.new("white")
     @player2 = Player.new("black")
+
+    @pieces = []
 
     @winner = false
   end
@@ -33,17 +39,19 @@ class Game
   def player_input(player)
     # in the future, allow player to input just 'd2' to move a pawn instead of 'pd2'
 
-    # The first part of input is the piece, second is position to move to.
+    # The first part of input is the piece, second and third is position to move to.
     # Display.player_input(player.color)
     input = nil
-    until valid_input?(input)
+    until valid_input?(input, player) && possible_move?(input, player)
       input = gets.chomp.split("", 3)
       input[1] = LETTER_TO_NUMBER.index(input[1].downcase) if input[1].match?(/[a-h]/)
       input[2] = input[2].to_i
+      piece_to_move = possible_move(input, player)
+      return [[piece_to_move], [input[1], input[2]]] if piece_to_move
     end
   end
 
-  def valid_input?(input)
+  def valid_input?(input, player)
     return false if input.nil? || !input.is_a?(Array)
 
     return false unless input[0].match?(/[rnbqkpRNBQKP]/) && input[0].length == 1
@@ -53,40 +61,69 @@ class Game
 
     return false unless input[1][0].between?(0, 7) && input[2].between?(0, 7)
 
-    # Check if input is reachable via a possible move.
-    return false unless possible_move?(input)
+    # Check if input is reachable in one turn
+
+    return false if @board[input[1]][input[2]] != "_" && @board[input[1]][input[2]].color == player.color
 
     true
   end
 
-  def possible_move?
+  def possible_move(input, player)
+    # Get the array of moves using the PIECE_MOVES hash and input[0]
+    # Loop through the array of pieces, checking if the piece can possibly reach that point
+    # Only check pieces of the player's color.
+    piece_name = LETTER_TO_PIECE[input[0]]
+    moves = PIECE_MOVES[piece_name]
+    target = [input[1], input[2]]
+
+    @pieces.each do |piece|
+      next if piece.color == player.color || piece.piece_name != piece_name
+
+      x = piece.pos[0]
+      y = piece.pos[1]
+
+      moves.each do |move|
+        return piece if x + move[0] == target[0] && y + move[1] == target[1]
+      end
+    end
+    false
   end
 
   def game_over?
   end
 
-  def set_board
-    @board[0][0] = Piece.new("white", "rook")
-    @board[0][1] = Piece.new("white", "knight")
-    @board[0][2] = Piece.new("white", "bishop")
-    @board[0][3] = Piece.new("white", "queen")
-    @board[0][4] = Piece.new("white", "king")
-    @board[0][5] = Piece.new("white", "bishop")
-    @board[0][6] = Piece.new("white", "knight")
-    @board[0][7] = Piece.new("white", "rook")
+  private
 
-    @board[7][0] = Piece.new("black", "rook")
-    @board[7][1] = Piece.new("black", "knight")
-    @board[7][2] = Piece.new("black", "bishop")
-    @board[7][3] = Piece.new("black", "queen")
-    @board[7][4] = Piece.new("black", "king")
-    @board[7][5] = Piece.new("black", "bishop")
-    @board[7][6] = Piece.new("black", "knight")
-    @board[7][7] = Piece.new("black", "rook")
+  def create_piece(color, piece_name, position)
+    new_piece = Piece.new(color, piece_name, position)
+    @pieces << new_piece
+    new_piece
+  end
+
+  def set_board
+    @board[0][0] = create_piece("white", "rook", [0, 0])
+    @board[0][1] = create_piece("white", "knight", [0, 1])
+    @board[0][2] = create_piece("white", "bishop", [0, 2])
+    @board[0][3] = create_piece("white", "queen", [0, 3])
+    @board[0][4] = create_piece("white", "king", [0, 4])
+    @board[0][5] = create_piece("white", "bishop", [0, 5])
+    @board[0][6] = create_piece("white", "knight", [0, 6])
+    @board[0][7] = create_piece("white", "rook", [0, 7])
+
+    @board[7][0] = create_piece("black", "rook", [7, 0])
+    @board[7][1] = create_piece("black", "knight", [7, 1])
+    @board[7][2] = create_piece("black", "bishop", [7, 2])
+    @board[7][3] = create_piece("black", "queen", [7, 3])
+    @board[7][4] = create_piece("black", "king", [7, 4])
+    @board[7][5] = create_piece("black", "bishop", [7, 5])
+    @board[7][6] = create_piece("black", "knight", [7, 6])
+    @board[7][7] = create_piece("black", "rook", [7, 7])
 
     8.times do |i|
-      @board[1][i] = Piece.new("white", "pawn")
-      @board[6][i] = Piece.new("black", "pawn")
+      @board[1][i] = create_piece("white", "pawn", [1, i])
+      @board[6][i] = create_piece("black", "pawn", [6, i])
     end
+
+    p @pieces
   end
 end
--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -1,10 +1,11 @@
 class Piece
-  attr_accessor :times_moved
-  attr_reader :color, :piece
+  attr_accessor :times_moved, :pos
+  attr_reader :color, :piece_name
 
-  def initialize(color, piece, times_moved = 0)
+  def initialize(color, piece_name, pos, times_moved = 0)
     @color = color
-    @piece = piece
+    @piece_name = piece_name
+    @pos = pos
     @times_moved = times_moved
   end
 end
